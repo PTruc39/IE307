045c534fd22057ac38e2a19c77f0fcf60f3444bd
PTruc39 <112930075+PTruc39@users.noreply.github.com>
2022-12-25 23:20:57 +0700

    post + show comment

---
+++ MangaApp/MangaApp/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaApp.Models
+{
+    public class Comment
+    {
+        public int MangaID { get; set; }
+        public int userID { get; set; }
+        public string userName { get; set; }
+        public string comment { get; set; }
+        public int commentID { get; set; }
+    }
+}
---
+++ MangaApp/MangaApp/Views/CommentPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MangaApp.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MangaApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommentPage : ContentPage
+    {
+        Host host = new Host();
+        HttpClient client = new HttpClient();
+        public CommentPage(Manga manga)
+        {
+            InitializeComponent();
+            testing.Text = manga.MangaName;
+            this.Manga=manga;
+        }
+
+        public Manga Manga { get; }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Comment cmt = new Comment();
+           
+            cmt.MangaID = this.Manga.MangaID;
+            cmt.userID = User.userID;
+            cmt.comment = Name.Text;
+            var json = JsonConvert.SerializeObject(cmt);
+            var noidung = new StringContent(json, Encoding.UTF8, "application/json");
+            var apires = await client.PostAsync(host.url + "api/user/AddComment", noidung);
+        }
+    }
+}
