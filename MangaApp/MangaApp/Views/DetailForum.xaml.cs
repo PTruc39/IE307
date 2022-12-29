1c76cc3fab17416f5f8cb1e7e4c2df75a8165043
PTruc39 <112930075+PTruc39@users.noreply.github.com>
2022-12-30 02:55:53 +0700

    get cmt by blog

--- MangaApp/MangaApp/Views/DetailForum.xaml.cs
+++ MangaApp/MangaApp/Views/DetailForum.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using MangaApp.Models;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +14,16 @@ namespace MangaApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailForum : ContentPage
     {
+        public List<Comment> cmt;
+        HttpClient http = new HttpClient();
+        Host host = new Host();
+        async void GetComment(Blogs blog)
+        {
+            var kq = await http.GetStringAsync
+                (host.url + "api/comment/GetCommentByManga?BlogID=" + blog.BlogID.ToString());
+            cmt = JsonConvert.DeserializeObject<List<Comment>>(kq);
+            lstdslh.ItemsSource = cmt;
+        }
         public DetailForum(Blogs blog)
         {
             InitializeComponent();
@@ -19,6 +31,11 @@ namespace MangaApp.Views
             BlogImg.Source = blog.BlogImg;
             BlogContent.Text = blog.BlogContent;
             User.Text = blog.userName;
+            GetComment(blog);
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
 
         }
     }
