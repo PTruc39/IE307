7ddee253296733a67fd235598bad7949b6e6d21b
nhung20521725 <pthnhunggt@gmail.com>
2023-01-09 00:22:25 +0700

    okayy

--- MangaApp/MangaApp/Models/Host.cs
+++ MangaApp/MangaApp/Models/Host.cs
@@ -9,13 +9,7 @@ namespace MangaApp
 
         //public string url = "http://172.30.208.45/MangaApi/";
 
-<<<<<<< HEAD
-        //public string url = "http://192.168.1.69/MangaApi/";
-=======
         public string url = "http://mangaappapi.somee.com/";
->>>>>>> 944165687b3da758593449551132b9b88c4e0e9c
-
-
 
     }
 }
