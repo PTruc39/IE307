990b0b92d412a10119d062b3560d293db4b61f81
nhung20521725 <pthnhunggt@gmail.com>
2023-01-09 15:57:03 +0700

    nhung

--- MangaApp/MangaApp/Views/ForumPage.xaml.cs
+++ MangaApp/MangaApp/Views/ForumPage.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using MangaApp.Models;
 using Newtonsoft.Json;
 using Xamarin.Forms;
